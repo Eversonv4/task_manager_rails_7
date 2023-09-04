32056645f5c4c34a023717d77cbf83eb2f98dc91
Everson Soares <eversonv4@gmail.com>
2023-09-04 17:50:30 -0300

    added function to export tasks to csv file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  include Exportable
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "admin" && password == "eve123"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   root "tasks#index"
-  resources :tasks, except: [:show]
-  # index
-  # show
-  # new
-  # create
-  # edit
-  # update
-  # destroy
+  resources :tasks, except: [:show] do 
+    collection do 
+      get :export_csv
+    end
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
